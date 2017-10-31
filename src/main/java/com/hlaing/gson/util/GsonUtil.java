a795525991a4b1aa6884751cbe9ffe17dc480bda
hlaingwintun <hlaingwintun@hlaingwintun-PC>
2017-10-31 23:52:25 +0800

    Test

--- src/main/java/com/hlaing/gson/util/GsonUtil.java
+++ src/main/java/com/hlaing/gson/util/GsonUtil.java
@@ -1,6 +1,7 @@
 package com.hlaing.gson.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.hlaing.gson.entity.Product;
 
 public class GsonUtil {
@@ -10,6 +11,20 @@ public class GsonUtil {
 		String json = gson.toJson(product);
 		System.out.println(json);
 		return json;
+	}
+	
+	public static String simpleJsonWithPrettyPrinting(final Product product) {
+		final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(product);
+		System.out.println(json);
+		return json;
+	}
+	
+	public static Product jsonToObject(final String json) {
+		Gson gson = new Gson();
+		Product product = gson.fromJson(json, Product.class);
+		System.out.println(product);
+		return product;
 	}
 
 }
--- src/test/java/com/hlaing/gson/GsonUtilTest.java
+++ src/test/java/com/hlaing/gson/GsonUtilTest.java
@@ -1,6 +1,7 @@
 package com.hlaing.gson;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 import java.math.BigDecimal;
 
@@ -13,12 +14,19 @@ import com.hlaing.gson.util.GsonUtil;
 public class GsonUtilTest {
 	private Product product;
 	
+	private String json = "{"
+			+ "\"productId\": \"235268845711068312\","
+			+ "\"description\": \"Spring Framework Guru Mug\","
+			+ "\"imageUrl\": \"https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_mug-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg\","
+			+ "\"price\": 14.00"
+			+ "}";
+	
 	@BeforeMethod
 	public void setUp() {
 		product = new Product("P01","Spring Guru Mug","https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg",new BigDecimal(18.95));
 	}
 	
-	@Test
+	@Test (enabled = false)
 	public void simpleJson() {
 		String result = GsonUtil.simpleJson(product);
 		
@@ -27,6 +35,22 @@ public class GsonUtilTest {
 	    assertEquals("\"description\":\"Spring Guru Mug\"".trim(), result.replace("{", "").replace("}","").split(",")[1].trim());
 	    assertEquals("\"imageUrl\":\"https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg\"".trim(), result.replace("{", "").replace("}","").split(",")[2].trim());
 	    assertEquals("\"price\":"+ new BigDecimal(18.95)+"".trim(), result.replace("{", "").replace("}","").split(",")[3].trim());
+	}
+	
+	@Test (enabled = false)
+	public void simpleJsonPrettyPrinting() {
+		String result = GsonUtil.simpleJsonWithPrettyPrinting(product);
+	}
+	
+	@Test (enabled = true)
+	public void jsonToObjectTest() {
+		final Product product = GsonUtil.jsonToObject(json);
+		
+		  assertNotNull(product);
+		  assertEquals("235268845711068312", product.getProductId());
+		  assertEquals("Spring Framework Guru Mug", product.getDescription());
+		  assertEquals("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_towel-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg", product.getImageUrl());
+		  assertEquals(new BigDecimal(14), product.getPrice());
 	}
 
 }
